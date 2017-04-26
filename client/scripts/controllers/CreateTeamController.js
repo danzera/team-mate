16a67c5033fb247ac93d7af04e0af22f81c62e92
Dan Zera <dczera@gmail.com>
2017-04-26 14:04:19 -0500

    Add view to create a team

---
+++ client/scripts/controllers/CreateTeamController.js
@@ -0,0 +1,5 @@
+myApp.controller('CreateTeamController', [function() {
+  console.log('hello from the CreateTeamController');
+  let createTeam = this;
+  createTeam.name = 'hello';
+}]);
--- client/scripts/factories/UserService.js
+++ client/scripts/factories/UserService.js
@@ -1,5 +1,5 @@
 myApp.factory('UserService', ['$http', '$location', function($http, $location){
-  // instantiate a new userObject
+  // instantiate a new userObject on factory load
   let userObject = new User();
   console.log('initial userObject in the factory:', userObject);
   // get user from the database
@@ -25,15 +25,20 @@ myApp.factory('UserService', ['$http', '$location', function($http, $location){
             $location.path("/home"); // WILL WANT TO CHANGE THIS TO TEAMS VIEW
         }
     });
-  }
+  } // end getUser()
 
+  // logout the user
   function logout() {
     $http.get('/user/logout').then(function(response) {
       console.log('you logged out - see you next time');
       $location.path("/home");
     });
-  }
+  } // end logout()
 
+  // IF A REDIRECT IS NEEDED -- USE $location
+  // NOT SURE IF A FUNCTION SPECIFICALLY FOR THE ACTION IS A GOOD THOUGHT
+  // BUT DOES SEEM TO MAKE CODE MORE READABLE
+  // NEED TO EXPORT IT IF IT'S TO BE USED BY CONTROLLERS
   // function redirectToLogin() {
   //   $location.path("/login");
   // }
@@ -42,6 +47,5 @@ myApp.factory('UserService', ['$http', '$location', function($http, $location){
     userObject,
     getUser,
     logout
-    // redirectToLogin
   };
 }]);
