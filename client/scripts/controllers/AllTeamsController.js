f4b53d5e070b8f825cfb078001b45b2e7fef40a6
Dan Zera <dczera@gmail.com>
2017-05-01 17:47:30 -0500

    add alert if user is not a member of a team

--- client/scripts/controllers/AllTeamsController.js
+++ client/scripts/controllers/AllTeamsController.js
@@ -1,5 +1,4 @@
 myApp.controller('AllTeamsController', ['UserService', function(UserService) {
   let allTeams = this;
   allTeams.userObject = UserService.userObject;
-  console.log('users teams in AllTeamsController', allTeams.userObject.getTeamsArray());
 }]);
