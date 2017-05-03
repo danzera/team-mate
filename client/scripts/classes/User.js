524c5a260d4f404154f50640f4fa5dc94f497b48
Dan Zera <dczera@gmail.com>
2017-05-03 14:32:28 -0500

    clean up user class - remove unnecessary current team info - this has been moved to the teamStatusObject on the user

--- client/scripts/classes/User.js
+++ client/scripts/classes/User.js
@@ -1,15 +1,12 @@
 // user class
 class User {
-  constructor(id, username = '', firstName = '', lastName = '', phone = '', teamStatusObject = {}, currentTeamId = '', hasJoined = false, isManager = false) {
+  constructor(id, username = '', firstName = '', lastName = '', phone = '', teamStatusObject = {}) {
     this.id = id; // user id from the database
     this.username = username; // username === username in the database
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
     this.teamStatusObject = teamStatusObject;
-    //this.currentTeamId = currentTeamId;
-    //this.hasJoined = hasJoined;
-    //this.isManager = isManager;
   }
   // clear all User properties
   clear() {
@@ -19,9 +16,6 @@ class User {
     this.lastName = '';
     this.phone = '';
     this.teamStatusObject = {};
-    this.currentTeamId = '';
-    this.hasJoined = false;
-    this.isManager = false;
   }
   // get the user's ID - matches 'id' in the 'users' table of the database
   getId() {
@@ -77,29 +71,5 @@ class User {
     this.teamStatusObject[teamId].teamName = teamName;
     this.teamStatusObject[teamId].hasJoined = hasJoined;
     this.teamStatusObject[teamId].isManager = isManager;
-  }
-  // get the user's currentTeamId ID
-  getCurrentTeamId() {
-    return this.currentTeamId;
-  }
-  // set the user's currentTeamId ID
-  setCurrentTeamId(teamId) {
-    this.currentTeamId = teamId;
-  }
-  // get the user's username - matches 'username' in the 'users' table of the database
-  getHasJoined() {
-    return this.hasJoined;
-  }
-  // set the user's username - used to assign the user username (username) that is returned from the database after user is authenticated
-  setHasJoined(hasJoined) {
-    this.hasJoined = hasJoined;
-  }
-  // get the user's manager status for their currentTeamId
-  getIsManager() {
-    return this.isManager;
-  }
-  // set the user's manager status for their currentTeamId
-  setIsManager(isManager) {
-    this.isManager = isManager;
   }
 }
